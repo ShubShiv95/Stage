20c264de3f261d66a63f0abf937831c61a02f28c
Shubham Shivam <ShubShiv95.swipe>
2022-02-15 14:36:53 +0530

    Student Bulk Import Changes

--- ImportAllStudentData_2.php
+++ ImportAllStudentData_2.php
@@ -4,6 +4,7 @@
    include 'AdmissionModel.php';
    include 'security.php';
    include 'errorLog.php';   
+   require_once 'sequenceGenerator.php';
 
    mysqli_autocommit($dbhandle,FALSE);
 
@@ -12,7 +13,7 @@
    $fileHandle = fopen($file, "r");
    $counter = 0;
    $isFieldMissing[] = true; //dummy data
-   $htmlbody = '<div class="table-responsive"><table class="table table-bordered"><th>Student Id</th><th>School ID</th><th>Session</th><th>Session Start Year</th><th>Session End Year</th><th>First Name</th><th>Middle Name</th><th>Last Name </th><th>Class Id</th><th>Class Sec</th><th>Roll No.</th><th>Gender</th><th>DOB</th><th>Discount Category</th><th>Father Name</th><th>Mother Name</th><th>Guardian Name</th><th>SMS ContactNo</th><th>Student Type</th><th>Stream</th><th>Enabled</th></tr></thead>';
+   $htmlbody = '<div class="table-responsive"><table class="table table-bordered"><th>Student Id</th><th>School ID</th><th>Session</th><th>Session Start Year</th><th>Session End Year</th><th>First Name</th><th>Middle Name</th><th>Last Name </th><th>Class Id</th><th>Class Sec</th><th>Roll No.</th><th>Gender</th><th>DOB</th><th>Discount Category</th><th>Father Name</th><th>Mother Name</th><th>Guardian Name</th><th>SMS ContactNo</th><th>Student Type</th><th>Stream</th><th>Enabled</th><th>Communication</th><th>State</th><th>Nationality</th><th>Mother Tongue</th></tr></thead>';
    $htmlbody = $htmlbody . '<tbody>';
 
    
@@ -195,6 +196,42 @@
                      : $htmlbody . '<td>' . $enabled . '</td>';
       $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
 
+
+      $dataArray[$counter][21] = $filesop[21];
+      $enabled = $filesop[21];
+      $isMandatory = array_search("COMMUNICATION",$GLOBAL_ADMISSION_FIELD_IS_MANDATORY);
+      $htmlbody = ($isMandatory && empty($enabled)) ? $htmlbody . '<td style="background-color: red">' . " " . '</td>'
+                     : $htmlbody . '<td>' . $enabled . '</td>';
+      $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
+
+      $dataArray[$counter][22] = $filesop[22];
+      $enabled = $filesop[22];
+      $isMandatory = array_search("STATE",$GLOBAL_ADMISSION_FIELD_IS_MANDATORY);
+      $htmlbody = ($isMandatory && empty($enabled)) ? $htmlbody . '<td style="background-color: red">' . " " . '</td>'
+                     : $htmlbody . '<td>' . $enabled . '</td>';
+      $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
+
+      $dataArray[$counter][23] = $filesop[23];
+      $enabled = $filesop[23];
+      $isMandatory = array_search("NATIONALITY",$GLOBAL_ADMISSION_FIELD_IS_MANDATORY);
+      $htmlbody = ($isMandatory && empty($enabled)) ? $htmlbody . '<td style="background-color: red">' . " " . '</td>'
+                     : $htmlbody . '<td>' . $enabled . '</td>';
+      $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
+
+      $dataArray[$counter][24] = $filesop[24];
+      $enabled = $filesop[24];
+      $isMandatory = array_search("MOTHER_TONGUE",$GLOBAL_ADMISSION_FIELD_IS_MANDATORY);
+      $htmlbody = ($isMandatory && empty($enabled)) ? $htmlbody . '<td style="background-color: red">' . " " . '</td>'
+                     : $htmlbody . '<td>' . $enabled . '</td>';
+      $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
+      $htmlbody = $htmlbody . '</tr>';
+	   
+	  $dataArray[$counter][25] = $filesop[25];
+      $enabled = $filesop[25];
+      $isMandatory = array_search("SCHOOL_ADM_NO",$GLOBAL_ADMISSION_FIELD_IS_MANDATORY);
+      $htmlbody = ($isMandatory && empty($enabled)) ? $htmlbody . '<td style="background-color: red">' . " " . '</td>'
+                     : $htmlbody . '<td>' . $enabled . '</td>';
+      $isFieldMissing []= ($isMandatory && empty($enabled)) ? false : true ; 
       $htmlbody = $htmlbody . '</tr>';
 
 		$counter++;
@@ -209,7 +246,6 @@
    else{  
    //########################   Else Block in case of validation passes and now data insert will begin  ################################### 
       $testelse ="";
-      $schoolCode = "DPS";
       $updatedBy = $_SESSION["LOGINID"];
       $IsTransSuccess = true;
       $errorAreaMessage = "";
@@ -218,16 +254,15 @@
       Session_Start_Year, Session_End_Year, School_Id, Student_Type, Stream, Enabled, Concession_Id, Updated_By) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
       
       $insertStudentTableSql = "insert into student_master_table
-      (Student_Id, School_Id, Session, Session_Start_Year, Session_End_Year, First_Name, Middle_Name, Last_Name, Class_Id, Class_Sec_Id, Gender, DOB, Father_Name,
-      Mother_Name, Guardian_Name, SMS_Contact_No, Updated_By) 
-      values(?,?,?,?,?,?,?,?,?,?,?,str_to_date(?,'%d/%m/%Y'),?,?,?,?,?)";
+      (Student_Id, School_Adm_No, School_Id, Session, Session_Start_Year, Session_End_Year, First_Name, Middle_Name, Last_Name, Class_Id, Class_Sec_Id, Gender, DOB, Father_Name,
+      Mother_Name, Guardian_Name, SMS_Contact_No, Date_Of_Enroll,Student_Login_Id, Parent_Login_Id, Enabled, Comm_Address, Comm_Add_State, Nationality, Mother_Tongue, Updated_By) 
+      values(?,?,?,?,?,?,?,?,?,?,?,?,str_to_date(?,'%d-%m-%Y'),?,?,?,?,str_to_date(?,'%d/%m/%Y'),?,?,?,?,?,?,?,?)";
       
-
       $tempArray = array();
       $size = count($dataArray);
       $testVariable ="";
       $i = 1;
-      
+
       for($i = 1; $i < $size; $i++)
       {
          $datarow = "";
@@ -239,7 +274,10 @@
           $studClassNo = $tempArray[9];
           $schoolId = $tempArray[2];
           $rollNo = $tempArray[11];
+          //Change for CISDHN
+          $student_id = $tempArray[1];
           $IsTransSuccess = true;
+          $enabled = $tempArray[21];
 
           //########################   Below Block of SQL Gets Class ID from Class_Master Table  ###################################
           $studentClassIdSql = "Select class_id as Class_Id from class_master_table where Class_Name='" . $studClassNo . "'" . "and School_Id = '" . $schoolId. "'" ;
@@ -272,22 +310,13 @@
 
           //########################   Below Block of SQL updates Student_Class_Details Table  ###################################
 
-         //Below Block of SQL calculates the Next value of Student_Class_detail index.
-
-          $studentClassDetIdCountSql = "Select count(Student_Details_Id) as studDetailId from student_class_details where school_id='" . $schoolId. "'";
-          $studentClassDetCountResult = $dbhandle->query($studentClassDetIdCountSql);
-    
-          if($studentClassDetCountResult)
-          {
-             $getStudCountRow = $studentClassDetCountResult -> fetch_assoc();
-             $student_Detail_Id = ($getStudCountRow["studDetailId"] + 1);
-          }  
-                   
+          $student_cl_gen = max_sequence_number('student_class_details',"Student_Details_Id", $dbhandle);
+          $today_date = date("d/m/Y");
+          //$student_id = generate_student_id('student_master_table', $dbhandle);
           $stmt = $dbhandle -> prepare($insertStudentClassTableSql);
-    
-          $stmt->bind_param("isiiiisiiissiis",   
-          $student_Detail_Id,
-          $tempArray[1],
+          $stmt->bind_param("isiiiisiiissiis",
+          $student_cl_gen,   
+          $student_id,
           $studClassId,
           $studClassNo,
           $studSecsId,
@@ -306,15 +335,65 @@
           $execResult = $stmt->execute();
           echo $dbhandle->error;
 
+          if(!$execResult){
+            $IsTransSuccess = false;
+            $errorAreaMessage = "Unable to insert data in Student_Class_Details Table student_id =" . $student_id ;
+            mysqli_rollback($dbhandle);
+            $error_msg= $errorAreaMessage;
+            $sql='';
+            $el = new LogMessage();
+            $el->write_log_message('Unable to Import Student Data', $error_msg, $sql,__FILE__, $_SESSION['LOGINID']);
+            break;
+         }
+
          //############################    End of Student_Class_Details update.   #####################################
 
+         //######################   Populate Parent login detail to the Login Table ##################################
 
+         $parent_login_id = $tempArray[18];
+         // checking login table for parent login
+         $parent_check = "SELECT * FROM login_table WHERE LOGIN_ID = ?";
+         $parent_check_prep = $dbhandle->prepare($parent_check);
+         $parent_check_prep->bind_param("s",$parent_login_id);
+         $parent_check_prep->execute();
+         $result_p_login = $parent_check_prep->get_result();
+         $parent_login_data = $result_p_login->fetch_assoc();
+         //$password = rand(); // For temporary basis Password is kept as DDMMYYYY format.
+         $password = str_replace('-', '', $tempArray[13]);
+         $enc_pwd = sha1($password);
+         if (empty($parent_login_data['LOGIN_ID'])) {
+           $parent_login_id = $tempArray[18];
+           // insert into login table for parent
+           //$login_id = sequence_number('login_table',$dbhandle);
+			$login_id=max_sequence_number('login_table','LID',$dbhandle);
+           $parent_login = "insert into login_table (LID, LOGIN_ID, PASSWORD, LOGIN_TYPE, MOBILE_NO, Role_Id, Role_Approved, SCHOOL_ID) VALUES (?,?,?,?,?,?,?,?)";
+           $l_type = 'PARENT';
+           $role_Id = "2";
+           $role_Approved = "1";
+           $parent_login_prep = $dbhandle->prepare($parent_login);
+           $parent_login_prep->bind_param("issssiii",$login_id,$parent_login_id,$enc_pwd,$l_type,$parent_login_id,$role_Id,$role_Approved,$schoolId);
+           $execResult = $parent_login_prep->execute();
+           echo $dbhandle->error;
+           if(!$execResult){
+            $IsTransSuccess = false;
+            $errorAreaMessage = "$login_id Unable to insert Parent Login Data in Login_Table LOGIN_ID = " . $parent_login_id ;
+            mysqli_rollback($dbhandle);
+            $error_msg= $errorAreaMessage;
+            $sql='';
+            $el = new LogMessage();
+            $el->write_log_message('Unable to Import Student Data', $error_msg, $sql,__FILE__, $_SESSION['LOGINID']);
+            break;
+          }
+         }else{
+           $parent_login_id = $parent_login_data['LOGIN_ID'];//$tempArray[18];
+         }
+         //########################  End of getting Parent Login Id  ######################################################
+         
          //########################   Below Block of SQL updates Student_Master_Table   ###################################
-
           $stmt2 = $dbhandle -> prepare($insertStudentTableSql);
-    
-          $stmt2->bind_param("sisiisssiisssssss",   
-          $tempArray[1],
+          $stmt2->bind_param("ssisiisssiisssssssssisssss",   
+          $student_id,
+          $tempArray[26],
           $schoolId,
           $tempArray[3],
           $tempArray[4],
@@ -325,36 +404,65 @@
           $studClassId,
           $studSecsId,
           $tempArray[12],
-          $tempArray[14],
+          $tempArray[13],
           $tempArray[15],
           $tempArray[16],
           $tempArray[17],
           $tempArray[18],
+          $today_date,
+          $student_id,
+          $parent_login_id, //parent id  = sms_contact_no
+          $enabled,
+          $tempArray[22],
+          $tempArray[23],
+          $tempArray[24],
+          $tempArray[25],
           $updatedBy          
          );
             
           $execResult = $stmt2->execute();
           echo $dbhandle->error;
-         
           if(!$execResult){
             $IsTransSuccess = false;
+            $errorAreaMessage = "Unable to insert data in Student_Master_Table student_id =" . $student_id ;
             mysqli_rollback($dbhandle);
+            break;
+          }
+         // // insert into login table
+          //$login_id = sequence_number('login_table',$dbhandle);
+		  $login_id=max_sequence_number('login_table','LID',$dbhandle);
+          $ins_st_login = "INSERT INTO login_table (LID, LOGIN_ID, PASSWORD, LOGIN_TYPE, MOBILE_NO, Role_Id, Role_Approved, SCHOOL_ID) VALUES (?,?,?,?,?,?,?,?)";
+          $l_type = 'STUDENT';
+          $role_Id = "2";
+          $role_Approve = "1";
+          $ins_st_login_prep = $dbhandle->prepare($ins_st_login);
+          $ins_st_login_prep->bind_param("issssiii",$login_id,$student_id,$enc_pwd,$l_type,$tempArray[18],$role_Id,$role_Approve,$schoolId);
+          $execResult = $ins_st_login_prep->execute();
+         if(!$execResult){
+            $IsTransSuccess = false;
+            $errorAreaMessage = "Unable to Insert Data in Login_Table while inserting Student Login Info Login_ID =" . $student_id ;
+            mysqli_rollback($dbhandle);
+            $error_msg= $errorAreaMessage;
+            $sql='';
+            $el = new LogMessage();
+            $el->write_log_message('Unable to Import Student Data', $error_msg, $sql,__FILE__, $_SESSION['LOGINID']);
             break;
          } 
 
       }//end of for
-   
      if($IsTransSuccess){
          mysqli_commit($dbhandle);
          //$dbhandle->query('UNLOCK TABLES');
-         echo "Successfully Data Imported into Student Master, Student Class Details tables.";
+         echo "<B>Successfully Data Imported into Login Table, Student Master, Student Class Details tables.</B>";
        } 
       else{
           mysqli_rollback($dbhandle);
+          $error_msg= $errorAreaMessage;
+          $sql='';
+          $el = new LogMessage();
+          $el->write_log_message('Unable to Import Student Data', $error_msg, $sql,__FILE__, $_SESSION['LOGINID']);
+          die;
          //$dbhandle->query('UNLOCK TABLES');
          echo "Sorry! Unable to import  |  Error Area :- " . "<br>". $errorAreaMessage;
       }   
    }//end of else
-   
-
-?>
